4cc5e4ddb73a467af69270f63df202487d2cd7ab
vipra ramtekkar <vipra.r@gmail.com>
2021-04-16 08:36:00 +0200

    Initial commit

---
+++ src/main/java/com/abn/recipe/pRecipeApi/PRecipeApiApplication.java
@@ -0,0 +1,13 @@
+package com.abn.recipe.pRecipeApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PRecipeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PRecipeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abn/recipe/pRecipeApi/PRecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abn.recipe.pRecipeApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PRecipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
